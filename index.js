555c1a9c65495561327613c96fdd8854c52aa476
Dennis Muensterer <dennismuensterer@gmail.com>
2021-09-19 16:58:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var markdownpdf = require("markdown-pdf")
+fs = require("fs")
+
+// File I/O
+const TITLE = "markdown"
+const INPUT_PATH = `./in/${TITLE}.md`
+const OUTPUT_PATH = `./out/${TITLE}.pdf`
+
+// Config
+const CSS_NAME = "custom"
+var options = {
+    paperOrientation: 'landscape',
+    cssPath: `./css/${CSS_NAME}.css`,
+    remarkable: {
+        html: true,
+        breaks: true,
+        plugins: [ require('remarkable-emoji') ],
+		    syntax: [ 'footnote', 'sup', 'sub' ]
+    }
+}
+
+markdownpdf(options)
+  .from(INPUT_PATH)
+  .to(OUTPUT_PATH, function () { console.log("Done") })
